1280739cb43d1ebcd563c9da1a2052f25cd99c74
giuseppefurcolo <me@giuseppefurcolo.com>
2024-07-12 10:58:53 +0200

    fix

--- code.js
+++ code.js
@@ -3,13 +3,9 @@ figma.showUI(__html__);
 figma.ui.onmessage = async msg => {
   if (msg.type === 'paste-text') {
     const { text } = msg;
-    const lines = text.split('\n');
+    const lines = text.split('\n').filter(line => line.trim() !== "");
 
     const selectedTextNodes = figma.currentPage.selection.filter(node => node.type === 'TEXT');
-    if (selectedTextNodes.length < lines.length) {
-      figma.notify('The number of selected text layers is less than the number of lines.');
-      return;
-    }
 
     for (let i = 0; i < selectedTextNodes.length; i++) {
       const textNode = selectedTextNodes[i];
