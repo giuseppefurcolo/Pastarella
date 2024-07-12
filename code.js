213423ae870e2e43bab176f0377c18a8f159ef4e
giuseppefurcolo <me@giuseppefurcolo.com>
2024-07-12 10:51:36 +0200

    fix

--- code.js
+++ code.js
@@ -6,15 +6,16 @@ figma.ui.onmessage = async msg => {
     const lines = text.split('\n');
 
     const selectedTextNodes = figma.currentPage.selection.filter(node => node.type === 'TEXT');
-    if (selectedTextNodes.length !== lines.length) {
-      figma.notify('The number of selected text layers does not match the number of lines.');
+    if (selectedTextNodes.length < lines.length) {
+      figma.notify('The number of selected text layers is less than the number of lines.');
       return;
     }
 
-    for (let i = 0; i < lines.length; i++) {
+    for (let i = 0; i < selectedTextNodes.length; i++) {
       const textNode = selectedTextNodes[i];
+      const line = lines[i % lines.length];
       await figma.loadFontAsync(textNode.fontName);
-      textNode.characters = lines[i];
+      textNode.characters = line;
     }
 
     figma.closePlugin();
